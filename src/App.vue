129febb5fb9e4573a53fe10db74d00a51a38f707
Titouan <titouan.pellerin@gmail.com>
2021-02-24 22:31:35 +0100

    Navbar start

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,133 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <div class="nav-wrapper">
+      <div class="socials-menu">
+        <SocialIcon
+          icon="fa-github"
+          link="https://github.com/titouan-pellerin"
+        ></SocialIcon>
+        <SocialIcon
+          icon="fa-facebook-f"
+          link="https://www.facebook.com/titouan.pellerin"
+        ></SocialIcon>
+        <SocialIcon
+          icon="fa-linkedin-in"
+          link="https://www.linkedin.com/in/titouan-pellerin-24352b1a1/"
+        ></SocialIcon>
+      </div>
+      <router-link to="/">Accueil</router-link>
+      <router-link to="/works">Créations</router-link>
+      <router-link class="menu-logo" to="/"
+        ><img alt="Logo Titouan" src="./assets/logo.svg"
+      /></router-link>
+      <router-link to="/contact">Contact</router-link>
+      <router-link to="/contact">Contact</router-link>
+      <a class="btn visufo-cta" href="https://visufo.fr" target="_blank"
+        >Visufo</a
+      >
+    </div>
   </div>
-  <router-view/>
+  <router-view />
 </template>
+<script>
+import SocialIcon from "@/components/SocialIcon.vue";
+
+export default {
+  components: {
+    SocialIcon,
+  },
+};
+</script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+:root {
+  --accent-color: #439efe;
+  --darker-accent-color: #3884d4;
+}
+
+html {
+  font-family: "Poppins", "sans-serif";
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+}
+
+body {
+  margin: 0;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  font-family: "EB Garamond", "serif";
+}
+
+::selection {
+  background: #439dfe6b;
+  color: #fff;
+}
+
+.btn {
+  background-color: var(--accent-color);
+  padding: 10px 30px;
+  border-radius: 50px;
+  transition: background-color ease-in-out 0.2s;
+}
+
+.btn:hover {
+  background-color: var(--darker-accent-color);
 }
 
 #nav {
-  padding: 30px;
+  background-color: #000;
+  padding: 10px 30px;
+}
+
+.nav-wrapper {
+  text-transform: uppercase;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
 }
 
 #nav a {
+  color: #fff;
   font-weight: bold;
-  color: #2c3e50;
+  text-decoration: none;
+  font-weight: 400;
+  transition-duration: 0.2s;
+}
+
+#nav a:not(.social-link) {
+  margin: 0 20px;
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color: #439efe;
+}
+
+#nav a:not(.btn):hover {
+  color: #439efe;
+}
+
+.menu-logo img {
+  width: 100px;
+}
+
+.socials-menu,
+.visufo-cta {
+  position: absolute;
+}
+
+.socials-menu {
+  left: 0;
+}
+
+.visufo-cta {
+  right: 0;
 }
 </style>
---
+++ src/components/SocialIcon.vue
@@ -0,0 +1,30 @@
+<template>
+  <a class="social-link" :href="link" target="_blank"><i class="fab" :class="icon"></i></a>
+</template>
+
+<script>
+export default {
+  name: 'SocialIcon',
+  props: {
+    icon: String,
+    link: String
+  }
+}
+</script>
+
+<style>
+  .social-link{
+    color: #fff;
+    transition: color ease-in-out .2s;
+  }
+
+  .social-link:not(:last-child){
+    margin-right: 20px;
+  }
+
+  .social-link{
+    color: var(--acent-color);
+  }
+
+
+</style>
